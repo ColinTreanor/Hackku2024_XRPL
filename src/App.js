d053a9a8c41bde9b67eb867bcfe1ceb600a8fc87
kylespragg <kylesspragg@gmail.com>
2024-04-13 12:07:12 -0500

    Added visuals to Logo.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
 import './index.css';
+
+import KoalaArms from './Design/Koala_Arms.png';
+import KoalaBody from './Design/Koala_Body.png';
+import SproutLeft from './Design/Sprout1.png';
+import SproutRight from './Design/Sprout2.png';
+import SettingsIcon from './Design/Settings_Gear.png';
+
 import Settings from './Settings';
 import Main from './Main';
 import Login from './Login';
@@ -15,17 +22,25 @@ function App() {
   return (
     <div>
       <div id="koalaTitle">
+        <img src={KoalaArms} id="koalaArms"/>
+        <img src={KoalaBody} id="koalaBody"/>
+        <button id="settingsButton"><img src={SettingsIcon} alt="Logo" id="buttonPng"/>
+        </button>
         <p id="koalaText">Koala Wallet</p>
         <div id="headBorder"></div>
       </div>
       <div className="sideborders-container">
-        <div className="border-left"></div>
+        <div className="border-left">
+          <img src={SproutLeft} id="sproutLeft"/>
+        </div>
         <div className="content">
           {currentPage === 'Settings' && <Settings />}
           {currentPage === 'Main' && <Main />}
           {currentPage === 'Login' && <Login onPageChange={handlePageChange} />}
         </div>
-        <div className="border-right"></div>
+        <div className="border-right">
+          <img src={SproutRight} id="sproutRight"/>
+        </div>
       </div>
     </div>
   );
--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 const serverUrl = 'http://127.0.0.1:5000';
+
 /* 
 Functionality:
 
