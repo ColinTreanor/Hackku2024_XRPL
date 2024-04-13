1acf124b14084fd1c39355c6ec0c46c12faa2b1b
Alex Doehring <alex.doehring2@gmail.com>
2024-04-12 21:19:47 -0500

    Added create_new_wallet() func

--- wallet.py
+++ wallet.py
@@ -3,7 +3,11 @@ import json
 
 testnet_url = "https://s.devnet.rippletest.net:51234/"
 
-# client = xrpl.clients.JsonRpcClient(testnet_url)
-client = xrpl.wallet.generate_faucet_wallet(testnet_url)
 
-print(client)
+def create_new_wallet(key=''):
+    if key == '':
+        client = xrpl.clients.JsonRpcClient(testnet_url)
+        new_wallet = xrpl.wallet.generate_faucet_wallet(client)
+    else:
+        xrpl.wallet.Wallet.from_seed(key)
+    return new_wallet
