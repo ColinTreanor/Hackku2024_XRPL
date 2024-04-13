f0a1eb8f4233c999a5189f33fca7160b5ca127b8
Alex Doehring <alex.doehring2@gmail.com>
2024-04-12 22:18:58 -0500

    Added more functions

--- wallet.py
+++ wallet.py
@@ -14,8 +14,23 @@ def get_account(key):
     return wallet
     
 
-def get_info(account_id):
+def get_info(seed):
+    account_id = seed.address
     client = xrpl.clients.JsonRpcClient(testnet_url)
+    acct_info = xrpl.models.requests.account_info.AccountInfo(
+        account=account_id,
+        ledger_index="validated"
+    )
+    response = client.request(acct_info)
+    return response.result['account_data']
+
+def last_transaction(seed):
+    client = xrpl.clients.JsonRpcClient(testnet_url)
+    try:
+        last_transaction_obj = xrpl.account.get_latest_transaction(seed.address)
+    except xrpl.XRPLRequestFailureException:
+        print("ERROR: Transaction failed")
+    return last_transaction_obj
     
 def wallet_to_json(wallet):
     return {
@@ -24,4 +39,7 @@ def wallet_to_json(wallet):
         "public_key": wallet.public_key
     }
     
-# print(get_account("sEdVnyag2smPo1mY6Xxv8Go2L9LWVTg").private_key)
+# print(get_account("sEdVnyag2smPo1mY6Xxv8Go2L9LWVTg"))
+# print()
+# print(get_info(get_account("sEdVnyag2smPo1mY6Xxv8Go2L9LWVTg")))
+print(last_transaction("sEdVnyag2smPo1mY6Xxv8Go2L9LWVTg"))
