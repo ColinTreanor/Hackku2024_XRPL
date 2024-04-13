9a63ea5a221c4c0f960f352da90f8e78a328d317
kylespragg <kylesspragg@gmail.com>
2024-04-13 06:24:00 -0500

    Built App.js, starting functionality of Login.js (front-end and axios request)

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-
+import axios from 'axios';
 
 /* 
 Functionality:
@@ -22,14 +22,78 @@ Notes:
     HANDLE ERRORS
 
 */
+
+
 function Login() {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const [userInput, setUserInput] = useState('');
+  const [error, setError] = useState('');
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+  const handleChange = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  const loginSeed = async () => {
+    try {
+      if (!userInput || userInput.length !== 31) {
+        setError('Seed must be 31 characters long');
+        return;
+      }
+      const response = await axios.post('./api/wallet', { seed: userInput, value: 'newseed' });
+      console.log("Wallet created:", response.data);
+      // Handle successful response as needed
+    } catch (error) {
+      console.error('Error creating wallet:', error.message);
+      setError('Failed to create wallet');
+      // Handle error as needed
+    }
+  };
+  
 
   return (
     <div className="loginContainer">
-    <h1 id="loginHeader">Login</h1>
+      <h1 id="loginHeader">Login</h1>
+      <div className="inputContainer">
+        {/* Input field */}
+        <input
+          id="inputSeed"
+          className="input-seed"
+          type="number"
+          placeholder="Seed Entry"
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          value={userInput} // Set input value to userInput state
+          onChange={handleChange} // Handle input change
+        />
+        {/* Border element */}
+        <div id="positionBorder">
+          <div className={`inputBorder ${isFocused || isHovered ? 'focused' : ''}`}></div>
+        </div>
+      </div>
+      <div id="centerSubmitButton">
+      <button className="submitSeed" type="text"  onClick={loginSeed}>Create</button>
+      </div>
     </div>
-
   );
 }
 
 export default Login;
+
