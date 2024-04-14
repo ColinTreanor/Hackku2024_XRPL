69ddc422d290dbc0c0a4fe0a52f6128f5cc4f52a
kylespragg <kylesspragg@gmail.com>
2024-04-13 22:45:23 -0500

    Designed Settings.js file

--- src/Settings.js
+++ src/Settings.js
@@ -3,8 +3,27 @@ import React from 'react';
 
 function Settings() {
   return (
-    <div>
-      <h1>Shit is getting tested just differently</h1>
+    <div id="mainAccount">
+      <h1 id="mainAccountInfo">Account Information</h1>
+      <div id="alignPublicKey">
+        <h2 id="publicKeyText">Public Key:</h2>
+        <h2 id="publicKey">Mock Key</h2>
+      </div>
+      <div id="alignPrivateKey">
+        <h2 id="privateKeyText">Private Key:</h2>
+        <h2 id="privateKey">Mock Key</h2>
+      </div>
+      <div id="alignSeedKey">
+        <h2 id="seedKeyText">Seed Key:</h2>
+        <h2 id="seedKey">Mock Key</h2>
+      </div>
+      <div id="devLetter">
+        <p>Hello, this is a message from the devs!<br />Thank you
+        so much for utilization our application for your Crypto needs. Feel
+        free to use any of the features on this platform at your convienence. If you
+        have any questions, feel free to ask!</p>
+        <p id="socials">info@koalawalla.com</p>
+      </div>
     </div>
   );
 }
